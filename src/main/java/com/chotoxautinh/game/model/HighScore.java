868ec1124f9f639fe1d54d49805427cc06e1d556
TruongKhang <truongkhang95@gmail.com>
2016-04-24 01:56:44 +0700

    Update code HighScore

--- src/main/java/com/chotoxautinh/game/model/HighScore.java
+++ src/main/java/com/chotoxautinh/game/model/HighScore.java
@@ -6,5 +6,28 @@
 package com.chotoxautinh.game.model;
 
 public class HighScore {
-
+	
+	private String name; // Name of Player
+	private int score;	// score after game completed
+	
+	public HighScore(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
+	
 }
