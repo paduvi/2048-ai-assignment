d8033321c1d5ccc0bb4fe641833d6c6401068062
lavanquan <kenlvq@gmail.com>
2016-04-28 21:23:22 -0700

    abc

--- src/main/java/com/chotoxautinh/ai/GameAgent.java
+++ src/main/java/com/chotoxautinh/ai/GameAgent.java
@@ -1,5 +1,8 @@
 package com.chotoxautinh.ai;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import com.chotoxautinh.game.model.Board;
 import com.chotoxautinh.game.model.Direction;
 import com.chotoxautinh.game.model.Node;
@@ -26,7 +29,7 @@ public class GameAgent {
 	public void setDepth(int depth) {
 		this.depth = depth;
 	}
-
+	
 	private Node alphaBeta(Board board, int depth, boolean isPlayerTurn, int alpha, int beta) throws CloneNotSupportedException{
 		Node node = new Node();
 
@@ -43,14 +46,30 @@ public class GameAgent {
 		} else {
 			// alpha-beta pruning
 			if (isPlayerTurn) {
-				for (int i = 1; i < Direction.values().length; i++) {
+				ArrayList<Node> temp = new ArrayList<Node>();
+				for (int i = 0; i < Direction.values().length; i++) {
 					Board boardOfChildNode = (Board) board.clone();
 					Node childNode = new Node();
 					if (boardOfChildNode.canMove(Direction.values()[i])) {
 						boardOfChildNode.move(Direction.values()[i]);
+						childNode.setValueDirOfChild(Direction.values()[i]);
+						childNode.setActualScore(boardOfChildNode.getActualScore());
+						childNode.setParentMove(i);
+						this.setHeuristicScore(childNode, boardOfChildNode);
+						temp.add(childNode);
+					}
+				}
+				Collections.sort(temp);
+				
+				for (int i = 0; i < temp.size(); i++) {
+					Board boardOfChildNode = (Board) board.clone();
+					Node childNode = new Node();
+					int j = temp.get(i).getParentMove();
+					if (boardOfChildNode.canMove(Direction.values()[j])) {
+						boardOfChildNode.move(Direction.values()[j]);
 						childNode = alphaBeta(boardOfChildNode, depth - 1, false, alpha, beta);
 						alpha = Math.max(alpha, childNode.getValue());
-						childNode.setValueDirOfChild(Direction.values()[i]);
+						childNode.setValueDirOfChild(Direction.values()[j]);
 						childNode.setActualScore(boardOfChildNode.getActualScore());
 						node.appendChild(childNode);
 						if (beta <= alpha)
@@ -58,8 +77,10 @@ public class GameAgent {
 					}
 				}
 				node.setValue(alpha);
+				
 			} else {
 				int[] newvalue = { 2, 4 };
+				ArrayList<Node> temp = new ArrayList<Node>();
 				for (int emptyCellPos = 0; emptyCellPos < board.getNumberOfEmptyCells(); emptyCellPos++) {
 					Board boardOfChild = (Board) board.clone();
 
@@ -69,12 +90,23 @@ public class GameAgent {
 					for (int valueOfCell : newvalue) {
 						boardOfChild.setValueToCell(valueOfCell, row, col);
 						Node child = new Node();
-						child = alphaBeta(boardOfChild, depth - 1, true, alpha, beta);
-						beta = Math.min(beta, child.getValue());
-						node.appendChild(child);
-						if (beta <= alpha)
-							break;
+						this.setHeuristicScore(child, boardOfChild);
+						child.setValueOfCell(valueOfCell);
+						child.setCol(col);
+						child.setRow(row);
+						temp.add(child);
 					}
+				}
+				Collections.sort(temp);
+				
+				for (int i = temp.size()-1; i >= 0; i--) {
+					Board boardOfChild = (Board) board.clone();
+					Node newNode = temp.get(i);
+					boardOfChild.setValueToCell(newNode.getValueOfCell(), newNode.getRow(), newNode.getCol());
+					Node child = new Node();
+					child = alphaBeta(boardOfChild, depth - 1, true, alpha, beta);
+					beta = Math.min(beta, child.getValue());
+					node.appendChild(child);
 					if (beta <= alpha)
 						break;
 				}
@@ -87,6 +119,70 @@ public class GameAgent {
 
 		return node;
 	}
+
+//	private Node alphaBeta(Board board, int depth, boolean isPlayerTurn, int alpha, int beta) throws CloneNotSupportedException{
+//		Node node = new Node();
+//
+//		if (board.isTerminated()) {
+//			if (board.hasWon())
+//				node.setValue(Integer.MAX_VALUE);
+//			else {
+//				node.setValue(Math.min(board.getActualScore(), 0));
+//				node.setLeaf(true);
+//			}
+//		} else if (depth == 0) {
+//			this.setHeuristicScore(node, board);
+//			node.setLeaf(true);
+//		} else {
+//			// alpha-beta pruning
+//			if (isPlayerTurn) {
+//				for (int i = 1; i < Direction.values().length; i++) {
+//					Board boardOfChildNode = (Board) board.clone();
+//					Node childNode = new Node();
+//					if (boardOfChildNode.canMove(Direction.values()[i])) {
+//						boardOfChildNode.move(Direction.values()[i]);
+//						childNode = alphaBeta(boardOfChildNode, depth - 1, false, alpha, beta);
+//						alpha = Math.max(alpha, childNode.getValue());
+//						childNode.setValueDirOfChild(Direction.values()[i]);
+//						childNode.setActualScore(boardOfChildNode.getActualScore());
+//						node.appendChild(childNode);
+//						if (beta <= alpha)
+//							break;
+//					}
+//				}
+//				for (int i = 0; i < node.getChildren().size(); i++) {
+//					System.out.println("i = "+i+" value = "+node.getChildren().get(i).getValue());
+//				}
+//				node.setValue(alpha);
+//			} else {
+//				int[] newvalue = { 2, 4 };
+//				for (int emptyCellPos = 0; emptyCellPos < board.getNumberOfEmptyCells(); emptyCellPos++) {
+//					Board boardOfChild = (Board) board.clone();
+//
+//					int row = board.getEmptyCellIds().get(emptyCellPos) / 4;
+//					int col = board.getEmptyCellIds().get(emptyCellPos) % 4;
+//
+//					for (int valueOfCell : newvalue) {
+//						boardOfChild.setValueToCell(valueOfCell, row, col);
+//						Node child = new Node();
+//						child = alphaBeta(boardOfChild, depth - 1, true, alpha, beta);
+//						beta = Math.min(beta, child.getValue());
+//						node.appendChild(child);
+//						if (beta <= alpha)
+//							break;
+//					}
+//					if (beta <= alpha)
+//						break;
+//				}
+//				node.setValue(beta);
+//			}
+//		}
+//
+//		Direction bestDirection = this.findBestDirectionForPlayer(isPlayerTurn, node);
+//		node.setDirection(bestDirection);
+//
+//		return node;
+//	}
 
 	private Direction findBestDirectionForPlayer(boolean isPlayerTurn, Node node) {
 		Direction bestDirection = Direction.NONE;
--- src/main/java/com/chotoxautinh/game/model/Node.java
+++ src/main/java/com/chotoxautinh/game/model/Node.java
@@ -3,7 +3,7 @@ package com.chotoxautinh.game.model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Node {
+public class Node implements Comparable<Node> {
 
 	private int value;
 	private List<Node> children;
@@ -11,10 +11,15 @@ public class Node {
 	private Direction direction;
 	private Direction valueDirOfChild;
 	private int actualScore;
-	
+	private int parentMove;
+	private int row = 0;
+	private int col = 0;
+	private int valueOfCell = 0;
+
 	public Node() {
 		children = new ArrayList<Node>();
 	}
+
 	public int getValue() {
 		return value;
 	}
@@ -46,21 +51,60 @@ public class Node {
 	public void setDirection(Direction direction) {
 		this.direction = direction;
 	}
-	
+
 	public void setValueDirOfChild(Direction dir) {
 		this.valueDirOfChild = dir;
 	}
-	
+
 	public Direction getValueDirOfChild() {
 		return this.valueDirOfChild;
 	}
-	
+
 	public void setActualScore(int score) {
 		this.actualScore = score;
 	}
-	
+
 	public int getActualScore() {
 		return actualScore;
 	}
 
+	@Override
+	public int compareTo(Node node) {
+		// TODO Auto-generated method stub
+		int compareValue = ((Node) node).getValue();
+		return -(this.getValue() - compareValue);
+	}
+
+	public int getParentMove() {
+		return parentMove;
+	}
+
+	public void setParentMove(int parentMove) {
+		this.parentMove = parentMove;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+	public int getValueOfCell() {
+		return valueOfCell;
+	}
+
+	public void setValueOfCell(int valueOfCell) {
+		this.valueOfCell = valueOfCell;
+	}
+	
 }
